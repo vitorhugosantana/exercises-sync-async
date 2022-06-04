a6ef2a801ff99a9fc0a1f5865810b923733f308a
Felipe Rosas <contato@eufelipe.com>
2022-06-04 18:18:07 -0300

    chore: update comments

--- src/tasks.js
+++ src/tasks.js
@@ -9,15 +9,23 @@ Consulte a documentação da lib em https://github.com/Marak/colors.js
 */
 
 // TODO 1: Essa Task deve retornar uma promise e com delay de 10 segundos
-const task1 = () => {};
+const task1 = () => {
+  // Deve retornar a mensagem: Terminou a Task 1 em 10 segundos
+};
 
 // TODO 2: Essa Task deve retornar uma promise e com delay de 5 segundos
-const task2 = () => {};
+const task2 = () => {
+  // Deve retornar a mensagem: Terminou a Task 2 em 5 segundos
+};
 
 // TODO 3: Essa Task deve retornar uma promise e com delay de 4 segundos
-const task3 = () => {};
+const task3 = () => {
+  // Deve retornar a mensagem: Terminou a Task 3 em 4 segundos
+};
 
 // TODO 4: Essa Task deve retornar uma promise e com delay de 4 segundos
-const task4 = () => {};
+const task4 = () => {
+  // Deve retornar a mensagem: Terminou a Task 4 em 4 segundos
+};
 
 module.exports = { task1, task2, task3, task4 };
