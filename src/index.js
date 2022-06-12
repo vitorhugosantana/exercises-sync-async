b546e18281e2cd4d68fbeea01f095327a99196bb
vitorhugosantana <santana.vitor91@gmail.com>
2022-06-12 19:02:21 -0300

    Adicionado scripts de tasks sincronas e assincronas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 const { task1, task2, task3, task4 } = require("./tasks");
 
-const synchronousExecution = () => {
+const synchronousExecution = async () => {
+  console.log ('sincrono')
+  await task1()
+  await task2()
+  await task3()
+  await task4()
+
   // TODO 1: Implemente aqui o código para executar as tarefas de forma SÍNCRONA
 };
 
 const asynchronousExecution = () => {
+  console.log ('assincrono')
+  task1()
+  task2()
+  task3()
+  task4()
+  
   // TODO 2: Implemente aqui o código para executar as tarefas de forma ASSÍNCRONA
 };
 
@@ -20,3 +32,10 @@ const asynchronousExecution = () => {
 
 
 */
+
+var input=process.argv[2]
+if (input === 'sync')
+  synchronousExecution()
+else 
+  asynchronousExecution()
+
--- src/tasks.js
+++ src/tasks.js
@@ -9,22 +9,54 @@ Consulte a documentação da lib em https://github.com/Marak/colors.js
 */
 
 // TODO 1: Essa Task deve retornar uma promise e com delay de 10 segundos
-const task1 = () => {
+const task1 = () => { 
+
+    return new Promise (resolve => 
+      setTimeout (
+        () => { 
+          console.log ('Terminou a Task 1 em 10 segundos'.red); 
+          resolve(); 
+        }
+      ,10000))
   // Deve retornar a mensagem: Terminou a Task 1 em 10 segundos
 };
 
 // TODO 2: Essa Task deve retornar uma promise e com delay de 5 segundos
 const task2 = () => {
+
+  return new Promise (resolve =>
+     setTimeout (
+       ()=> { 
+         console.log ('Terminou a Task 2 em 5 segundos'.magenta); 
+         resolve();
+         }
+        ,5000))
   // Deve retornar a mensagem: Terminou a Task 2 em 5 segundos
 };
 
 // TODO 3: Essa Task deve retornar uma promise e com delay de 4 segundos
 const task3 = () => {
+
+  return new Promise (resolve =>
+     setTimeout (
+       ()=> { 
+         console.log ('Terminou a Task 3 em 4 segundos'.cyan);
+         resolve() 
+        }
+        ,4000))
   // Deve retornar a mensagem: Terminou a Task 3 em 4 segundos
 };
 
 // TODO 4: Essa Task deve retornar uma promise e com delay de 4 segundos
 const task4 = () => {
+
+  return new Promise (resolve =>
+     setTimeout (
+       ()=> { 
+         console.log ('Terminou a Task 4 em 4 segundos'.green); 
+         resolve() 
+        }
+        ,4000))
   // Deve retornar a mensagem: Terminou a Task 4 em 4 segundos
 };
 
